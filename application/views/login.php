4ca20097fac6e5dae3d295c07fdaf6214d270f35
carreno <carreno@carreno>
2014-09-02 23:10:33 -0500

    register_view_ui

--- application/views/login.php
+++ application/views/login.php
@@ -44,45 +44,54 @@
             $("#signin").hide('fade');
             $("#new").show('fade');
         });
+        $("#back_login").click(function(e){
+            e.preventDefault();
+            $("#new").hide('fade');
+            $("#signin").show("fade");
+        });
+        
+        $("#register-form").submit(function(){
+            
+        });
     });
 </script>
-<br>
 <br>
 <form class="form-horizontal" id="signin"  method="post" action="<?php echo site_url(array("login","in")) ?>" role="form">
     <br>
     <h1 class="text-center"><b>BIENVENIDO</b></h1>
     <br>
-  <div class="form-group">
-    <label for="inputEmail3" class="col-md-3 control-label">Email</label>
-    <div class="col-md-7">
-        <div class="input-group">
-          <input class="form-control" type="text" placeholder="Enter email">
-            <div class="input-group-addon">@upa.edu.mx</div>
+    <div class="form-group">
+        <label for="inputEmail3" class="col-md-3 control-label">Email</label>
+        <div class="col-md-7">
+            <div class="input-group">
+                <input class="form-control" type="text" placeholder="Enter email" autofocus>
+                <div class="input-group-addon">@upa.edu.mx</div>
+            </div>
         </div>
     </div>
-  </div>
-  <div class="form-group">
-    <label for="inputPassword3" class="col-md-3 control-label">Password</label>
-    <div class="col-md-7">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    <div class="form-group">
+        <label for="inputPassword3" class="col-md-3 control-label">Password</label>
+            <div class="col-md-7">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+            </div>
     </div>
-  </div>
-  <!--<div class="form-group">
-    <div class="col-sm-offset-2 col-md-4">
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Remember me
-        </label>
-      </div>
+    <!--<div class="form-group">
+        <div class="col-sm-offset-2 col-md-4">
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"> Remember me
+                </label>
+            </div>
+        </div>
+    </div>-->
+    
+    <div class="form-group">
+        <div class="col-md-offset-3 col-md-10">
+            <button type="submit" class="btn btn-primary">Ingresar</button>
+        </div>
     </div>
-  </div>-->
-  <div class="form-group">
-    <div class="col-md-offset-3 col-md-10">
-      <button type="submit" class="btn btn-default">Ingresar</button>
-    </div>
-  </div>
-  <br>
-  <button type="button" class="pull-right btn btn-info" style="margin-right: 5%" id="registro"><b>Registrarse</b></button>
+    
+    <button type="button" class="pull-right btn btn-info" style="margin-right: 5%" id="registro"><b>Registrarse</b></button>
 </form>
 
 
@@ -90,19 +99,68 @@
 <!--Formulario de Registro-->
 <div id="new">
     <div class="row">
-        <div class="col-sm-6 col-md-4 col-md-offset-4">
-            <div class="account-wall">
-                <form class="form-signin" id="register-form"  method="post" action="<?php echo site_url(array("login","registro")) ?>" role="form">
-                    Elija un nombre de usuario:
-                    <input name="nombre" type="text" class="form-control" placeholder="Usuario" required autofocus>
-                    Elija una contraseña
-                    <input name="password" type="password" class="form-control" placeholder="Contraseña" required>
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">
-                    Enviar</button>
-                </form>
-            </div>
+        <div class="form-horizontal">
+            <form class="form-signin" id="register-form"  method="post" action="<?php echo site_url(array("login","registro")) ?>" role="form">
+                <br>
+                <h1 class="text-center"><b>REGISTRESE</b></h1>
+                <br>
+                <div class="form-group">
+                    <label for="inputPassword3" class="col-md-3 control-label">Nombre(s)</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" id="input Password3" placeholder="Nombre(s)" autofocus required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword3" class="col-md-3 control-label">Apellido Paterno</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" id="inputPassword3" placeholder="Apellido Paterno" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword3" class="col-md-3 control-label">Apellido Materno</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" id="inputPassword3" placeholder="Apellido Materno" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-md-3 control-label">Correo Institucional</label>
+                    <div class="col-md-7">
+                        <div class="input-group">
+                            <input class="form-control" type="text" placeholder="Correo Institucional">
+                            <div class="input-group-addon">@upa.edu.mx</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword3" class="col-md-3 control-label">Correo Personal</label>
+                    <div class="col-md-7">
+                        <input type="email" class="form-control" id="inputPassword3" placeholder="Correo Personal">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword3" class="col-md-3 control-label">Contraseña</label>
+                    <div class="col-md-7">
+                        <input type="password" class="form-control" id="inputPassword3" placeholder="Contraseña" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword3" class="col-md-3 control-label">Repetir Contraseña</label>
+                    <div class="col-md-7">
+                        <input type="password" class="form-control" id="inputPassword3" placeholder="Repetir contraseña" required>
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="col-md-offset-3 col-md-10">
+                        <button type="submit" class="btn btn-primary">Ingresar</button>
+                    </div>
+                </div>
+            </form>
+            <button type="button" class="pull-right btn btn-info" style="margin-right: 5%" id="back_login"><b>Ya estoy registrado</b></button>
         </div>
     </div>
 </div>
+<br>
+<br>
 
 <div id="error" class="alert alert-danger">Usuario o contraseña invalidos</div>
