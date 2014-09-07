69aa7e0779a018823b9cd1cc135f6e665e0cf80a
mitsiu-carreno <mitsiu-carreno@users.noreply.github.com>
2014-09-06 21:34:51 -0500

    test_5

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -12,7 +12,7 @@ class Login extends CI_Controller {
             $data["tipo"]= $this->user_model->get_type_by_iduser($id);
             echo $data;
         }
-        var_dump("test_4");
+        var_dump("test_5");
         $this->load->view("header");
         $this->load->view("login/login");
         
