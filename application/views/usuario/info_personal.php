67d15c2cd69a4fc20ad7f27c0cd440f9a8649b07
Mitsiu A. Carreno Sarabia <carreno@carreno>
2014-09-18 23:15:53 -0500

    check responsive

--- application/views/usuario/info_personal.php
+++ application/views/usuario/info_personal.php
@@ -4,11 +4,16 @@
 	}
 </style>
 <div class="row">
+	<div class="form-group">
+                    <label for="inputPassword3" class="col-md-3 control-label">Nombre(s)</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" autofocus required value="Mitsiu Alejandro">
+                    </div>
+                </div>
   <div class="col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
   <div class="clearfix visible-xs-block"></div>
   <div class="col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
 
-  <!-- Add the extra clearfix for only the required viewport -->
   <div class="clearfix visible-xs-block"></div>
 
   <div class="col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
