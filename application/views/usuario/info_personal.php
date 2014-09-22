c0cb72a611d30ddac3b7af45c9a2c79198052312
mitsiu-carreno <mitsiu.carreno@gmail.com>
2014-09-21 21:48:40 -0500

    Form-responsive-testing

--- application/views/usuario/info_personal.php
+++ application/views/usuario/info_personal.php
@@ -2,29 +2,48 @@
 	*{
 		color:black;
 	}
+  #lay{
+    background-color: red;
+  }
+  #in{
+    background-color: green;
+  }
 </style>
-<div class="row">
-	<!--<div class="form-group">
-                    <label for="inputPassword3" class="col-md-3 control-label">Nombre(s)</label>
-                    <div class="col-md-7">
-                        <input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" autofocus required value="Mitsiu Alejandro">
-                    </div>
-                </div>
-              -->
-  <div class="col-xs-6 col-sm-3"><input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" required value="Mitsiu Alejandro"></div>
-  <div class="clearfix visible-xs-block"></div>
+
+ <div class="row">        
+  <label class="col-md-2 col-sm-2">Nombre(s)</label>     
+  <div class="visible-md-block"></div>
   
-  <div class="col-xs-6 col-sm-3"><input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" required value="Mitsiu Alejandro"></div>
-  <div class="clearfix visible-xs-block"></div>
+  <div class="col-md-4 col-sm-4">
+    <input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" autofocus required value="Mitsiu Alejandro">
+  </div>
+  <div class="visible-md-block"></div>
 
-  <div class="col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
-  <div class="clearfix visible-xs-block"></div>
+  <label class="col-md-2 col-sm-2">Nombre(s)</label>     
+  <div class="visible-md-block"></div>
+  
+  <div class="col-md-4 col-sm-4">
+    <input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" autofocus required value="Mitsiu Alejandro">
+  </div>
+  <div class="visible-md-block"></div>
 
-  <div class="col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
-  <div class="clearfix visible-xs-block"></div>
+  <label class="col-md-2 col-sm-2">Nombre(s)</label>     
+  <div class="visible-md-block"></div>
+  
+  <div class="col-md-4 col-sm-4">
+    <input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" autofocus required value="Mitsiu Alejandro">
+  </div>
+  <div class="visible-md-block"></div>
 
-  <div class="col-xs-6 col-sm-3">.col-xs-6 .col-sm-3</div>
-   <div class="clearfix visible-xs-block"></div>
+  <label class="col-md-2 col-sm-2">Nombre(s)</label>     
+  <div class="visible-md-block"></div>
+  
+  <div class="col-md-4 col-sm-4">
+    <input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" autofocus required value="Mitsiu Alejandro">
+  </div>
+  <div class="visible-md-block"></div>
+
+  
 </div>
 
 <a href="<?php echo site_url("login/out")?>"><button type="button">Salir</button></a>
