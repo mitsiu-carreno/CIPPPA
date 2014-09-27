7c8905d2ee3333739078ba16e25e65f523799530
mitsiu-carreno <mitsiu.carreno@gmail.com>
2014-09-27 13:41:52 -0500

    Redesign de formularios login-registro

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -8,7 +8,7 @@
 <script>
     $(function(){
         $("#error").hide();
-        $("#new").hide();
+        $("#register_form").hide();
         $("#error_registro").hide();
 
         //ENVIAR FORMULARIO SIGN IN
@@ -30,12 +30,12 @@
         
         $('#registro').click(function(e){
             e.preventDefault();
-            $("#signin").hide('fade');
-            $("#new").show('fade');
+            $("#signin_form").hide('fade');
+            $("#register_form").show('fade');
         });
         $("#back_login").click(function(e){
             e.preventDefault();
-            $("#new").hide('fade');
+            $("#register_form").hide('fade');
             $("#signin").show("fade");
         });
         
@@ -63,7 +63,7 @@
             $.post(this.action, $(this).serialize(), function(data){
                 console.log(data);
                 if(data > 0)
-                    window.location = "<?php echo site_url(array("main", "info_personal")) ?>";
+                    window.location = "<?php echo site_url(array("main")) ?>";
                 else{
                     $("#error").show("fade");
                         setTimeout(function(){
@@ -78,8 +78,10 @@
 <br>
 <div id="error" class="alert alert-danger">Usuario o contraseña invalidos</div>
 <div id="error_registro" class="alert alert-danger">Las contraseñas no coinciden</div>
-<div class="login_wrapper">
-    <form id="signin" method="post" acction="<?php echo site_url(array("login", "in"))?>" role="form">
+
+<!--Formulario-Login-->
+<div class="login_wrapper" id="signin_form">
+    <form id="signin" method="post" action="<?php echo site_url(array("login", "in"))?>" role="form">
         <h1 class="text-center"><b>BIENVENIDO</b></h1>
         <div class="row">
             <div class="col-sm-1"></div>
@@ -89,7 +91,7 @@
         </div>
         <div class="row">
             <div class="col-sm-1"></div>
-            <div class="col-md-9">
+            <div class="col-sm-9">
                 <div class="input-group">
                     <input class="form-control" type="text" name="correo_institucion" placeholder="Enter email" autofocus>
                     <div class="input-group-addon">@upa.edu.mx</div>
@@ -104,127 +106,144 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-1"></div>
-            <div class="col-md-9">
+            <div class="col-sm-1">   </div>
+            <div class="col-sm-9">
                 <input type="password" class="form-control" name="password" placeholder="Password">
             </div>
         </div>
         <br>    
         <br>
-        <button type="submit" class="btn btn-primary">Ingresar</button>
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Ingresar</button>
+            </div>
+        </div>
+        
         <br>
         <br>
     </form>    
     <button type="button" class="pull-right btn btn-info" style="margin-right: 5%" id="registro"><b>Registrarse</b></button>
 </div>
-<!--
-<form class="login_wrapper" id="signin"  method="post" action="<?php echo site_url(array("login","in")) ?>" role="form">
-    <br>
-    <h1 class="text-center"><b>BIENVENIDO</b></h1>
-    <br>
-    <div class="form-group">
-        <label for="inputEmail3" class="col-md-3 control-label">Email</label>
-        <div class="col-md-7">
-            <div class="input-group">
-                <input class="form-control" type="text" name="correo_institucion" placeholder="Enter email" autofocus>
-                <div class="input-group-addon">@upa.edu.mx</div>
-            </div>
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="inputPassword3" class="col-md-3 control-label">Password</label>
-            <div class="col-md-7">
-                <input type="password" class="form-control" name="password" placeholder="Password">
-            </div>
-    </div>
-    <!--<div class="form-group">
-        <div class="col-sm-offset-2 col-md-4">
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox"> Remember me
-                </label>
-            </div>
-        </div>
-    </div>
-    
-    <div class="form-group">
-        <div class="col-md-offset-3 col-md-10">
-            <button type="submit" class="btn btn-primary">Ingresar</button>
-        </div>
-    </div>
-    
-    <button type="button" class="pull-right btn btn-info" style="margin-right: 5%" id="registro"><b>Registrarse</b></button>
-</form>
--->
 
+<!--Formulario-Registro-->
+<div id="register_form" class="login_wrapper"> 
+    <form id="register-form" method="post" action="<?php echo site_url(array("login","registro")) ?>" role="form">
+        <h1 class="text-center"><b>REGISTRESE</b></h1>
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-md-3">
+                <label>Nombre(s)</label>
+            </div>
+        </div>  
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-9">
+                <input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" autofocus required value="Mitsiu Alejandro">
+            </div>
+        </div>
+        <br>
 
-<!--Formulario de Registro-->
-<div id="new">
-    <div class="row">
-        <div class="login_wrapper">
-            <form class="form-signin" id="register-form"  method="post" action="<?php echo site_url(array("login","registro")) ?>" role="form">
-                <br>
-                <h1 class="text-center"><b>REGISTRESE</b></h1>
-                <br>
-                <div class="form-group">
-                    <label for="inputPassword3" class="col-md-3 control-label">Nombre(s)</label>
-                    <div class="col-md-7">
-                        <input type="text" class="form-control" name="nombre" placeholder="Nombre(s)" autofocus required value="Mitsiu Alejandro">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPassword3" class="col-md-3 control-label">Apellido Paterno</label>
-                    <div class="col-md-7">
-                        <input type="text" class="form-control" name="apellido_paterno" placeholder="Apellido Paterno" required value="Carreño">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPassword3" class="col-md-3 control-label">Apellido Materno</label>
-                    <div class="col-md-7">
-                        <input type="text" class="form-control" name="apellido_materno" placeholder="Apellido Materno" required value="Sarabia">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputEmail3" class="col-md-3 control-label">Correo Institucional</label>
-                    <div class="col-md-7">
-                        <div class="input-group">
-                            <input class="form-control" type="text" name="correo_institucion" id="registro_email_inst" placeholder="Correo Institucional" required value="mitsiu.carreno">
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-md-3">
+                <label>Apellido Paterno</label>
+            </div>
+        </div>          
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-9">
+                <input type="text" class="form-control" name="apellido_paterno" placeholder="Apellido Paterno" required value="Carreño">
+            </div>
+        </div>
+        <br>
+
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-md-3">
+                <label>Apellido Materno</label>
+            </div>
+        </div>  
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-9">
+                <input type="text" class="form-control" name="apellido_materno" placeholder="Apellido Materno" required value="Sarabia">
+            </div>
+        </div>
+        <br>
+
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-md-3">
+                <label>Correo Institucional</label>
+            </div>
+        </div>  
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-9">
+                <div class="input-group">
+                    <input class="form-control" type="text" name="correo_institucion" id="registro_email_inst" placeholder="Correo Institucional" required value="mitsiu.carreno">
                             <div class="input-group-addon">@upa.edu.mx</div>
-                        </div>
-                        <p>En caso que aún no cuente con un correo institucional deje este espacio en blanco</p>
-                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="inputPassword3" class="col-md-3 control-label">Correo Personal</label>
-                    <div class="col-md-7">
-                        <input type="email" class="form-control" name="correo_personal" id="registro_email_pers" placeholder="Correo Personal" required value="mitsiu.carreno@gmail.com">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPassword3" class="col-md-3 control-label">Contraseña</label>
-                    <div class="col-md-7">
-                        <input type="password" class="form-control" name="password" id="registro_pass" placeholder="Contraseña" required value="1234">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPassword3" class="col-md-3 control-label">Repetir Contraseña</label>
-                    <div class="col-md-7">
-                        <input type="password" class="form-control" name="password2" id="registro_pass2" placeholder="Repetir contraseña" required value="1234">
-                    </div>
-                </div>
-                
-                <div class="form-group">
-                    <div class="col-md-offset-3 col-md-10">
-                        <button id="form_reg_listo" type="submit" class="btn btn-primary">Registrarse</button>
-                    </div>
-                </div>
-            </form>
-            <button type="button" class="pull-right btn btn-info" style="margin-right: 5%" id="back_login"><b>Ya estoy registrado</b></button>
+                <p>En caso que aún no cuente con un correo institucional deje este espacio en blanco</p>
+            </div>
         </div>
-    </div>
+        <br>
+
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-md-3">
+                <label>Correo Personal</label>
+            </div>
+        </div>  
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-9">
+                <input type="email" class="form-control" name="correo_personal" id="registro_email_pers" placeholder="Correo Personal" required value="mitsiu.carreno@gmail.com">
+            </div>
+        </div>
+        <br>
+
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-md-3">
+                <label>Contraseña</label>
+            </div>
+        </div>  
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-9">
+                <input type="password" class="form-control" name="password" id="registro_pass" placeholder="Contraseña" required value="1234">
+            </div>
+        </div>
+        <br>
+
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-md-3">
+                <label>Repetir Contraseña</label>
+            </div>
+        </div>  
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-9">
+                <input type="password" class="form-control" name="password2" id="registro_pass2" placeholder="Repetir contraseña" required value="1234">
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-md-2">
+                <button id="form_reg_listo" type="submit" class="btn btn-primary">Registrarse</button>
+            </div>
+        </div>
+        <br>
+        <br>
+    </form>
+    <button type="button" class="pull-right btn btn-info" style="margin-right: 5%" id="back_login"><b>Ya estoy registrado</b></button>
 </div>
-<br>
-<br>
+
 <!-- //USAR EN CASO QUE SE PONGA LENTO
 <div id="cargando">
     <span>ESPERE</span>
