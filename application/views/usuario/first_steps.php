43d98859b8e7dd0ccbc77887578b3a90006a7b8d
mitsiu-carreno <mitsiu.carreno@gmail.com>
2014-10-01 16:21:49 -0500

    CSS of nav wizard

--- application/views/usuario/first_steps.php
+++ application/views/usuario/first_steps.php
@@ -21,13 +21,13 @@
 </script>
 <ul class='nav nav-wizard'>
   
-  <li><a href='#step1' data-toggle="tab" id="step1">Paso 1 - Información Personal</a></li>
+  <li class="active"><a href='#step1' data-toggle="tab" id="step1">Paso 1 - Información Personal</a></li>
 
-  <li class='active'><a href='#step2' data-toggle="tab" id="step2">Paso 2 - Datos Domiciliarios</a></li>
+  <li><a href='#step2' data-toggle="tab" id="step2">Paso 2 - Datos Domiciliarios</a></li>
 
-  <li><a id="step3">Paso 3 - Datos de Contacto</a></li>
+  <li><a href='#step3' data-toggle="tab" id="step3">Paso 3 - Datos de Contacto</a></li>
 
-  <li><a>Paso 4 - Fotografía</a></li>
+  <li><a href='#step4' data-toggle="tab" id="step4">Paso 4 - Fotografía</a></li>
   
   <button type="button" id="btn_salir_first_steps" class="btn btn-default pull-right">Salir</button>
   <button type="button" id="btn_salir_first_steps" class="btn btn-default pull-right">
